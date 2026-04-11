f8026750f8d70b1f44320fdefabf905908144b72
Zacky-Cherrat <Cherratzacky@gmail.com>
2026-04-11 11:38:18 -0400

    Update sensor.py

--- FollowGap/sensor.py
+++ FollowGap/sensor.py
@@ -1,12 +1,12 @@
 """
 SENSOR
 Gestion des capteurs.
-
+ 
 Rôle :
 - Lire les données du LiDAR (scan)
 - Lire les données IMU (yaw / orientation)
 - (Optionnel) Estimer la position (odometry)
-
+ 
 Sorties standard :
 - scan : liste de distances
 - yaw : orientation du robot
@@ -18,15 +18,15 @@ from adafruit_bno08x.i2c import BNO08X_I2C
 from adafruit_bno08x import (
 BNO_REPORT_LINEAR_ACCELERATION,
 BNO_REPORT_GAME_ROTATION_VECTOR,
-
+ 
 )
-
-
-
+ 
+ 
+ 
 def get_lidar_scan():
     """
     Retourne un scan LiDAR.
-
+ 
     Returns:
         scan (list of float):
             Liste de distances en mètres.
@@ -35,29 +35,22 @@ def get_lidar_scan():
             Exemple: scan[0] = -90°, scan[len/2] = 0°, scan[-1] = +90°
     """
     pass
-
-
-def dechu() -> tuple[np.ndarray,float]:
-    """Récupère les données du capteur inertielle (IMU)
-
-    Returns:
-        tuple[np.ndarray,float]: Un tuple contenant :
-            - accélération (np.ndarray): Matrice (3,1) d'accélération en m/s²
-            - yaw (float): Yaw du prototype en rad
-    """
-    # voir : https://cdn-learn.adafruit.com/downloads/pdf/adafruit-9-dof-orientation-imu-fusion-breakout-bno085.pdf
-    i2c = busio.I2C()
-    bno = BNO08X_I2C(i2c)
-    
-    bno.enable_feature(BNO_REPORT_GAME_ROTATION_VECTOR)
-    bno.enable_feature(BNO_REPORT_LINEAR_ACCELERATION)
-    
-    ax, ay, az = bno.linear_acceleration
-    qi, qj, qk, qr = bno.quaternion
-    
-    yaw = np.atan2(2 * (qr * qk + qi * qj, 1 - 2 * (qj**2 + qk**2)))
-    a = np.array([[ax],[ay],[az]])
+ 
+ 
+def IMU(bno) -> tuple[np.ndarray, float]:
+ 
+    try:
+        ax, ay, az = bno.linear_acceleration
+        qi, qj, qk, qr = bno.quaternion
+    except:
+        return np.zeros((3,1)), 0.0
+ 
+    yaw = np.atan2(
+        2 * (qr * qk + qi * qj),
+        1 - 2 * (qj**2 + qk**2)
+    )
+ 
+    a = np.array([[ax], [ay], [az]])
+ 
     return a, yaw
-
-def IMU(bno):
-    "allo"
+ 
